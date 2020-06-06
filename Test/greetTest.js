240a7dd10336da256d1f559e3b8a95849dbe1afa
Bantu1996 <sotyatobantu@gmail.com>
2020-06-07 01:57:32 +0200

    made more changes to my files.

---
+++ Test/greetTest.js
@@ -0,0 +1,16 @@
+describe("The Greetings factory function", function() {
+    describe("enter the name", function() {
+  
+  
+      it ("should be able to type any name", function () {
+  
+          let greetThem = GreetWithRespect();
+    
+          
+          greetThem.setNametToGreet(activeName); 
+          
+          assert.equal(greetThem.getNametToGreet(activeName),);
+    
+        });
+    })
+});
